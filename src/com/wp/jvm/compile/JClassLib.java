14e4d93071980b137ce555318ed539deb321878f
王萍 <wang.03.25@qq.com>
2017-12-01 22:58:19 +0800

    jclasslib修改class

---
+++ src/com/wp/jvm/compile/JClassLib.java
@@ -0,0 +1,15 @@
+package com.wp.jvm.compile;
+
+import java.util.HashMap;
+
+/**
+ * @author 王萍
+ * @date 2017/12/1 0001
+ */
+//JClassLib是一个查看class的工具，可视度较高，比javap输出的更容易理解。
+public class JClassLib {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+    }
+}
---
+++ src/com/wp/jvm/compile/Test.java
@@ -0,0 +1,44 @@
+package com.wp.jvm.compile;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class Test {
+    public static void main(String[] args) throws Exception {  
+  
+        String filePath = "E:\\download\\ExpService.class";  
+        FileInputStream fis = new FileInputStream(filePath);
+  
+        DataInput di = new DataInputStream(fis);
+        ClassFile cf = new ClassFile();  
+        cf.read(di);  
+          
+        CPInfo[] infos = cf.getConstantPool();  
+  
+        int count = infos.length;  
+        System.out.println(count);  
+          
+        for (int i = 0; i < count; i++) {  
+            if (infos[i] != null) {  
+                System.out.print(i);  
+                System.out.print(" = ");  
+                System.out.print(infos[i].getVerbose());  
+                System.out.print(" = ");  
+                System.out.println(infos[i].getTagVerbose());  
+                if (i == 10) {  
+                    //ConstantUtf8Info uInfo = (ConstantUtf8Info)infos[i];  
+                    //uInfo.setBytes("400".getBytes());               
+                    ConstantIntegerInfo uInfo=(ConstantIntegerInfo) infos[i];  
+                    uInfo.setInt(400);;  
+                    infos[i] = uInfo;  
+                }  
+            }  
+        }  
+          
+        cf.setConstantPool(infos);  
+        fis.close();  
+        File f = new File(filePath);  
+        ClassFileWriter.writeToFile(f, cf);  
+    }  
+}  
