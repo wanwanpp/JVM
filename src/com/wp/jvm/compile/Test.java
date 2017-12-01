985dae72aa4026956006e481a8c296877077a7b6
王萍 <wang.03.25@qq.com>
2017-12-01 23:05:21 +0800

    更新Test

--- src/com/wp/jvm/compile/Test.java
+++ src/com/wp/jvm/compile/Test.java
@@ -1,9 +1,14 @@
 package com.wp.jvm.compile;
 
+import org.gjt.jclasslib.io.ClassFileWriter;
+import org.gjt.jclasslib.structures.CPInfo;
+import org.gjt.jclasslib.structures.ClassFile;
+import org.gjt.jclasslib.structures.constants.ConstantIntegerInfo;
+
 import java.io.DataInput;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
-
 public class Test {
     public static void main(String[] args) throws Exception {  
   
@@ -38,7 +43,7 @@ public class Test {
           
         cf.setConstantPool(infos);  
         fis.close();  
-        File f = new File(filePath);  
+        File f = new File(filePath);
         ClassFileWriter.writeToFile(f, cf);  
     }  
 }  
